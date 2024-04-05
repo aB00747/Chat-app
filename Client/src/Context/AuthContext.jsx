b316dbc6c2607ecec1e4d85ac0b87892e33988a2
Abhishek Lodh <abhisheklodh007@gmail.com>
2024-04-06 00:43:33 +0530

    Create login user page

--- Client/src/Context/AuthContext.jsx
+++ Client/src/Context/AuthContext.jsx
@@ -13,56 +13,91 @@ export const AuthContextProvider = ({ children }) => {
     password: "",
   });
 
-  console.log("user",user);
+  const [loginError, setLoginError] = useState(null);
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
 
   useEffect(() => {
-
-    const user = localStorage.getItem("User") // get from localStorage
+    const user = localStorage.getItem("User"); // get from localStorage
     setUser(JSON.parse(user || null)); // set user
-
-  },[]);
-
+  }, []);
 
   const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info);
   }, []);
 
-  const registerUser = useCallback(async (e) => {
+  const updateLoginInfo = useCallback((info) => {
+    setLoginInfo(info);
+  }, []);
+
+  const registerUser = useCallback(
+    async (e) => {
       e.preventDefault();
 
-    setIsRegisterLoading(true);
-    setRegisterError(null);
+      setIsRegisterLoading(true);
+      setRegisterError(null);
 
-    const response = await postRequest(
-      `${baseUrl}/users/register`,
-      registerInfo
-    );
+      const response = await postRequest(
+        `${baseUrl}/users/register`,
+        registerInfo
+      );
 
-    setIsRegisterLoading(false);
+      setIsRegisterLoading(false);
 
-    if (response.error) {
-      setRegisterError(response);
-    }
+      if (response.error) {
+        setRegisterError(response);
+      }
 
-    // store  user data localStorage
-    localStorage.setItem("User", JSON.stringify(response));
-    setUser(response);
-  }, [ registerInfo ]);
+      // store  user data localStorage
+      localStorage.setItem("User", JSON.stringify(response));
+      setUser(response);
+    },
+    [registerInfo]
+  );
+
+  const loginUser = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      setIsLoginLoading(true);
+      setLoginError(null);
+
+      const response = await postRequest(`${baseUrl}/users/login`, loginInfo);
+
+      setIsLoginLoading(false);
+
+      if (response.error) {
+        setLoginError(response);
+      } else {
+        localStorage.setItem("User", JSON.stringify(response));
+        setUser(response);
+      }
+    },
+    [loginInfo]
+  );
 
   const logoutUser = useCallback(() => {
     localStorage.removeItem("User"); // Remove local strogae data
     setUser(null); // set User to null
-  },[]);
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
         user,
         registerInfo,
+        loginInfo,
         updateRegisterInfo,
+        updateLoginInfo,
         registerUser,
+        loginUser,
         registerError,
+        loginError,
         isRegisterLoading,
+        isLoginLoading,
         logoutUser,
       }}
     >
--- Client/src/Pages/Loginn.jsx
+++ Client/src/Pages/Loginn.jsx
@@ -1,5 +1,61 @@
+import { useContext, useState } from "react";
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../Context/AuthContext";
+
 const Loginn = () => {
-    return ( <>Loginn</> );
-}
- 
+    const {
+        loginInfo,
+        updateLoginInfo,
+        loginUser,
+        loginError,
+        isLoginLoading,
+      } = useContext(AuthContext); // set globally
+  return (
+    <>
+      <Form onSubmit={loginUser}>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "Center",
+            paddingTop: "12%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Login</h2>
+              {/* <h2>{user.name}</h2>  -- show Abhishek name*/}
+
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, email: e.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  updateLoginInfo({
+                    ...loginInfo,
+                    password: e.target.value,
+                  })
+                }
+              />
+              {loginError?.error && (
+                <Alert variant="danger">
+                  <p>{loginError?.errorMessage}</p>
+                </Alert>
+              )}
+              <Button variant="primary" type="submit">
+                {isLoginLoading ? "Loging..." : "Login"}
+              </Button>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
 export default Loginn;
