a8b56fdec20705ab856fd05fef1f12d442d57a1d
Abhishek Lodh <abhisheklodh007@gmail.com>
2024-04-05 23:50:09 +0530

    Created the logout and set, get and remove localstroage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./Pages/Chat";
 import Register from "./Pages/Register";
@@ -6,18 +6,20 @@ import Login from "./Pages/Loginn";
 import NavBar from "./Component/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css"; // inport  bootstrap css file. we can use bootstrp css with classname.
 import { Container } from "react-bootstrap";
+import { AuthContext } from "./Context/AuthContext";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const { user } = useContext(AuthContext);
 
   return (
     <>
     <NavBar />
     <Container className="text-secondary"> 
       <Routes>
-        <Route path="/" element={<Chat />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={user ? <Chat /> : <Login />}></Route>
+        <Route path="/register" element={user ? <Chat /> : <Register /> }></Route>
+        <Route path="/login" element={user ? <Chat/> : <Login />}></Route>
         {/* Make default route if not found any path it will redirect at this */}
         <Route path="*" element={<Navigate to="/" />}></Route>
       </Routes>
